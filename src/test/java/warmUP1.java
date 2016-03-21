a8f10a1a761e898e500e66b7081502b66c9375d9
pranaviddamsetty <pranaviddamsetty@gmail.com>
2016-03-21 13:04:58 -0700

    Initial commit

---
+++ src/test/java/warmUP1.java
@@ -0,0 +1,14 @@
+/**
+ * Created by pranaviddamsetty on 3/21/16.
+ */
+public class warmUP1 {
+
+    public static void main(String[] args) {
+
+        for(int i=1; i<101; i++)
+
+            System.out.println(i);
+
+        // Prints numbers 1 to 100//
+    }
+}
